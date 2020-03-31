81b076ba1b75118eff306268f29430f913011459
Tiago8067 <61700834+Tiago8067@users.noreply.github.com>
2020-03-31 20:13:04 +0100

    TiagoSoares-22340

--- Aula 5/PongGame-TiagoSoares-22340/Assets/Scripts/Ball.cs
+++ Aula 5/PongGame-TiagoSoares-22340/Assets/Scripts/Ball.cs
@@ -2,36 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ball : MonoBehaviour
+public class Ball : MonoBehaviour
 {
     public float speed;
     public Rigidbody2D rb;
 
-    private Vector2 startPosition;
+    public Vector2 startPosition;
 
     void Start()
     {
         launch();
-    {
-
-    void launch()
-    {
-        float x = Random.Range(0, 2) == 0 ? -1 : 1;
-        float y = Random.Range(0, 2) == 0 ? -1 : 1;
-        rb.velocity = new Vector2(speed * x, speed * y);
     }
-    void Reset()
+
+    private void launch()
+    {
+        float x = Random.Range(0, 2) == 0 ? -1 : 1; //Randomize the direction in x and y;
+        float y = Random.Range(0, 2) == 0 ? -1 : 1; //Randomize the direction in x and y;
+        rb.velocity = new Vector2(speed  x, speed  y);
+	}
+
+    public void Reset()
     {
         rb.velocity = Vector2.zero;
         transform.position = startPosition;
         launch();
-    }
+	}
 
-    void increaseSpeed()
+    public void increaseSpeed()
     {
-        float velx = rb.velocity[0] + (rb.velocity[0] > 0 ? 1 : -1);
-        float vely = rb.velocity[1] + (rb.velocity[1] > 0 ? 1 : -1);
+        float velx = rb.velocity[0] + (rb.velocity[0] > 0 ? 1 : -1); //Keep the same direction but increased speed;
+        float vely = rb.velocity[1] + (rb.velocity[1] > 0 ? 1 : -1); //Keep the same direction but increased speed;
 
-        rb.velocity = new Vector2(velx, vely);
-    }
-}}}
+        rb.velocity = new Vector2(velx, vely); //update speed based in initial speed;
+	}
+}
