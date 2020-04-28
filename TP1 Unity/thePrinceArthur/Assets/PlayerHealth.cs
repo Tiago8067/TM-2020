cbe16dc6c8b6ffbbcac133f1a33248849a2e0404
Tiago8067 <61700834+Tiago8067@users.noreply.github.com>
2020-04-28 17:16:04 +0100

    TP1_thePrinceArthur
    TiagoSoares_22340
    MiguelMonteiro_22063

---
+++ TP1 Unity/thePrinceArthur/Assets/PlayerHealth.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class PlayerHealth : MonoBehaviour
+{
+	public int curHealth;
+	public int maxHealth = 100;
+
+	private void Start()
+	{
+		curHealth = maxHealth;
+	}
+
+	private void Update()
+	{
+		if(curHealth > maxHealth)
+		{
+			curHealth = maxHealth;
+		}
+
+		if(curHealth <= 0)
+		{
+			Die();
+		}
+	}
+
+	void Die()
+	{
+		Application.LoadLevel(Application.loadedLevel);
+	}
+	public void Damage(int dmg)
+	{
+		curHealth -= dmg;
+	}
+	
+	public IEnumerator Knockback(float knockDur, float knockbackPwr, Vector3 knockbackDir)
+	{
+		float timer = 0;
+		while (knockDur > timer)
+		{
+			timer += Time.deltaTime;
+
+			//rb2d.AddForce(new Vector3(knockbackDir.x * -100, knockbackDir.y * knockbackPwr, transform.position.z));
+		}
+
+		yield return 0;
+	}  
+	
+	/*public int maxHealth = 100;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(20);
+        }
+        
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        healthBar.SetHealth(currentHealth);
+    }*/
+	
+	/*public int health = 100;
+
+	public GameObject deathEffect;
+
+	public void TakeDamage(int damage)
+	{
+		health -= damage;
+
+		StartCoroutine(DamageAnimation());
+
+		if (health <= 0)
+		{
+			Die();
+		}
+	}
+
+	void Die()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	}
+
+	IEnumerator DamageAnimation()
+	{
+		SpriteRenderer[] srs = GetComponentsInChildren<SpriteRenderer>();
+
+		for (int i = 0; i < 3; i++)
+		{
+			foreach (SpriteRenderer sr in srs)
+			{
+				Color c = sr.color;
+				c.a = 0;
+				sr.color = c;
+			}
+
+			yield return new WaitForSeconds(.1f);
+
+			foreach (SpriteRenderer sr in srs)
+			{
+				Color c = sr.color;
+				c.a = 1;
+				sr.color = c;
+			}
+
+			yield return new WaitForSeconds(.1f);
+		}
+	}*/
+
+}
